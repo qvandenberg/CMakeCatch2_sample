39e89bd8a338ed0e3cd7916d96b17ec89dc7b3b7
Quincy van den Berg <quincy.vandenberg@physics.ox.ac.uk>
2019-09-23 10:57:33 +0100

    pre-merge

---
+++ build/test/CMakeFiles/main_test.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Quincy/Documents/Code/Cpp_learnmaterial/catch2/cmake_standalone/tests/src/TrialClass.cpp" "/Users/Quincy/Documents/Code/Cpp_learnmaterial/catch2/cmake_standalone/tests/build/test/CMakeFiles/main_test.dir/__/src/TrialClass.cpp.o"
+  "/Users/Quincy/Documents/Code/Cpp_learnmaterial/catch2/cmake_standalone/tests/test/main_test.cpp" "/Users/Quincy/Documents/Code/Cpp_learnmaterial/catch2/cmake_standalone/tests/build/test/CMakeFiles/main_test.dir/main_test.cpp.o"
+  "/Users/Quincy/Documents/Code/Cpp_learnmaterial/catch2/cmake_standalone/tests/test/trial_class_test.cpp" "/Users/Quincy/Documents/Code/Cpp_learnmaterial/catch2/cmake_standalone/tests/build/test/CMakeFiles/main_test.dir/trial_class_test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../extern/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/Quincy/Documents/Code/Cpp_learnmaterial/catch2/cmake_standalone/tests/build/src/CMakeFiles/mainlib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/test/CMakeFiles/main_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main_test.dir/main_test.cpp.o"
+  "CMakeFiles/main_test.dir/trial_class_test.cpp.o"
+  "CMakeFiles/main_test.dir/__/src/TrialClass.cpp.o"
+  "../bin/main_test.pdb"
+  "../bin/main_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
